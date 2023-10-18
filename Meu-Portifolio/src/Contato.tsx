989688161358699516acfc0a2707766f36117568
Julio-at-377165117886 <julio@movanto.com.br>
2023-10-17 21:55:55 -0300

    apresentar email e contato de telefone

--- Meu-Portifolio/src/Contato.tsx
+++ Meu-Portifolio/src/Contato.tsx
@@ -4,6 +4,16 @@ function Contato() {
     return (
       <div className="Contato">      
       <h1>Entre em contato comigo</h1>
+
+      <div className="alinhar">
+      <div className='meios'>
+        <img className='contatoimg' src="src\img\Mail.png"/><p>julio.junior.301@gmail.com</p>      
+      </div>
+      <div className='meios'>
+       <img className='contatoimgzap' src="src\img\whatsapp.png"/><p>julio.junior.301@gmail.com</p>    
+      </div>
+      </div>
+      
       </div>
       );
 }
