35fa3504f7dd2397bddf9c208246d24b6de519ac
Julio-at-377165117886 <julio@movanto.com.br>
2023-10-27 14:45:30 -0300

    ajustes de Css

--- Meu-Portifolio/src/Contato.tsx
+++ Meu-Portifolio/src/Contato.tsx
@@ -10,7 +10,7 @@ function Contato() {
         <img className='contatoimg' src="src\img\Mail.png"/><p>julio.junior.301@gmail.com</p>      
       </div>
       <div className='meios'>
-       <img className='contatoimgzap' src="src\img\whatsapp.png"/><p>julio.junior.301@gmail.com</p>    
+       <img className='contatoimgzap' src="src\img\whatsapp.png"/><a href="https://wa.me/+5551992167233/?">+55(51)99216-7233</a>
       </div>
       </div>
       
